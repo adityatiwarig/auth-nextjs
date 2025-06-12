3a183a2e1722dc5c9d26b56717f3a690378924d5
adityatiwarig <aditya2461tiwari@gmail.com>
2025-06-12 22:46:42 +0530

    pages understanding

---
+++ src/dbConfig/dbConfig.ts
@@ -0,0 +1,27 @@
+import mongoose, { connection } from "mongoose";
+
+
+export async function connect() {
+    try {
+        mongoose.connect(process.env.MONGO_URI!);
+        const connection = mongoose.connection;
+
+
+        connection.on('connected ', () => {
+            console.log("Mongodb connected successfully");
+            
+        })
+
+        connection.on('error' , (err) =>{
+            console.log("Mongo connection error.Please make sure db is connected." + err);
+            process.exit();
+            
+        })
+        
+    } catch (error) {
+        console.log('Something went wrong');
+        console.log(error);
+        
+        
+    }
+}
