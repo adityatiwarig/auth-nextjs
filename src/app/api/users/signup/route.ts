c707d2ee9494d9ee38cab3e17d62d6a4dc5dfe2f
adityatiwarig <aditya2461tiwari@gmail.com>
2025-06-16 00:00:12 +0530

    changes

--- src/app/api/users/signup/route.ts
+++ src/app/api/users/signup/route.ts
@@ -2,6 +2,7 @@ import {connect} from "@/dbConfig/dbConfig";
 import User from "@/models/userModel";
 import { NextRequest, NextResponse } from "next/server";
 import bcryptjs from "bcryptjs";
+import { sendEmail } from "@/helper/mailer";
 
 
 
@@ -38,6 +39,8 @@ export async function POST(request: NextRequest){     // KHUD SE DALNA HAI
         // console.log(savedUser);
 
         //send verification email
+        await sendEmail({email , emailType:"VERIFY",
+        userId : savedUser._id})
 
 
         return NextResponse.json({           // FINAL RESPONSE BHEJNA HAI
--- src/app/api/users/verifyEmail/route.ts
+++ src/app/api/users/verifyEmail/route.ts
@@ -9,6 +9,33 @@ connect()
     try {
      const reqBody = await request.json()
      const {token} = reqBody   // FRONTEND SE HANDLE HOGA
+     console.log(token);
+
+     const user = await User.findOne({verifyToken: token,
+        verifyTokenExpiry :{$gt: Date.now()}  // vte greater rehna chahiye abhi se
+     })
+
+     if(!user){
+        return NextResponse.json({error:"Invalid Token"},
+            {status:400}
+        )
+     }
+
+     console.log(user);
+
+     user.isVerified = true;
+     user.verifyToken = undefined;
+     user.verifyTokenExpiry = undefined;
+     await user.save();
+
+     return NextResponse.json({
+        message:"Email verified successfully!",
+        success:true,
+     })
+     
+
+
+     
         
     } catch (error:any) {
         return NextResponse.json({error:error.message},{
